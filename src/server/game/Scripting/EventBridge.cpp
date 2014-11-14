208a4569dbb93770df692882363d7c5e877c2584
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2014-11-14 19:58:33 +0100

    Loading worldserver.conf to get RabbitMQ properties.

--- src/server/game/Scripting/EventBridge.cpp
+++ src/server/game/Scripting/EventBridge.cpp
@@ -37,6 +37,7 @@
 #include "DatabaseEnv.h"
 #include "World.h"
 #include "geohash.cpp"
+#include "Config.h"
 
 #include "mongo/bson/bson.h"
 #include <amqp_tcp_socket.h>
@@ -399,27 +400,31 @@ EventBridge::EventBridge()
     propsExpiration._flags |= AMQP_BASIC_EXPIRATION_FLAG;
     propsExpiration.expiration = expiration;
 
+    ConfigMgr cmgr;
+    std::string error;
+    cmgr.LoadInitial("worldserver.conf", error);
+    
     TC_LOG_INFO("server.loading", "Connecting to RabbitMQ: [%s,%d] (user: %s)",
-                sConfigMgr->GetStringDefault("RabbitMQ.host", "localhost").c_str(),
-                sConfigMgr->GetIntDefault("RabbitMQ.port", 5672),
-                sConfigMgr->GetStringDefault("RabbitMQ.user", "guest").c_str());
+                cmgr.GetStringDefault("RabbitMQ.host", "localhost").c_str(),
+                cmgr.GetIntDefault("RabbitMQ.port", 5672),
+                cmgr.GetStringDefault("RabbitMQ.user", "guest").c_str());
     
     socket = amqp_tcp_socket_new(connEvents);
     amqp_socket_open(socket,
-                     sConfigMgr->GetStringDefault("RabbitMQ.host", "localhost").c_str(),
-                     sConfigMgr->GetIntDefault("RabbitMQ.port", 5672));
+                     cmgr.GetStringDefault("RabbitMQ.host", "localhost").c_str(),
+                     cmgr.GetIntDefault("RabbitMQ.port", 5672));
     amqp_login(connEvents, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-               sConfigMgr->GetStringDefault("RabbitMQ.user", "guest").c_str(),
-               sConfigMgr->GetStringDefault("RabbitMQ.pass", "guest").c_str());
+               cmgr.GetStringDefault("RabbitMQ.user", "guest").c_str(),
+               cmgr.GetStringDefault("RabbitMQ.pass", "guest").c_str());
     amqp_channel_open(connEvents, 1);
     
     socket = amqp_tcp_socket_new(connActions);
     amqp_socket_open(socket,
-                     sConfigMgr->GetStringDefault("RabbitMQ.host", "localhost").c_str(),
-                     sConfigMgr->GetIntDefault("RabbitMQ.port", 5672));
+                     cmgr.GetStringDefault("RabbitMQ.host", "localhost").c_str(),
+                     cmgr.GetIntDefault("RabbitMQ.port", 5672));
     amqp_login(connActions, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
-               sConfigMgr->GetStringDefault("RabbitMQ.user", "guest").c_str(),
-               sConfigMgr->GetStringDefault("RabbitMQ.pass", "guest").c_str());
+               cmgr.GetStringDefault("RabbitMQ.user", "guest").c_str(),
+               cmgr.GetStringDefault("RabbitMQ.pass", "guest").c_str());
     amqp_channel_open(connActions, 2);
     
     pthread_create(&thread1, NULL, processActions, NULL);
